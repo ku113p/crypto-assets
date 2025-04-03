527112ce1a7616c7d8d9133e8ff19b4ac912c120
august346 <ku113p@gmail.com>
2025-04-03 16:16:42 +0800

    upd - static htmx | add row on start | add - allocations change

--- src/routers/allocations/htmx.rs
+++ src/routers/allocations/htmx.rs
@@ -5,20 +5,27 @@ use axum::response::Html;
 use serde::Deserialize;
 use crate::app_state::AppState;
 use crate::routers::allocations::methods::{AllocationStore, AllocationView};
+use crate::routers::utils;
 
 pub struct Methods;
 
 impl Methods {
     pub async fn list(State(app_state): State<Arc<AppState>>) -> Html<String> {
         let allocations = AllocationStore::new(app_state).list().await;
-        let html = allocations.into_iter().map(|a| {
-            format!(
-                "<tr id='allocation-{}-{}'><td>{}</td><td>{}</td><td>{}</td><td><button hx-delete='/allocations/{}/{}' hx-target='#allocation-{}-{}'>Delete</button></td></tr>",
-                a.scheme_name, a.symbol, a.scheme_name, a.symbol, a.amount, a.scheme_name, a.symbol, a.scheme_name, a.symbol
-            )
-        }).collect::<Vec<_>>().join("\n");
 
-        Html(format!("<table><tr><th>Scheme</th><th>Symbol</th><th>Amount</th><th>Actions</th></tr>{}</table>", html))
+        let row_template = utils::get_file_text("row_allocation.html").await;
+        let rows_html = allocations.into_iter()
+            .map(|a| row_template
+                .replace("{scheme_name}", &a.scheme_name)
+                .replace("{symbol}", &a.symbol)
+                .replace("{amount}", &a.amount.to_string()))
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        let list_template = utils::get_file_text("list_allocations.html").await;
+        let list_html = list_template.replace("{rows}", &rows_html);
+
+        Html(list_html)
     }
 
     pub async fn create_or_update(
@@ -27,10 +34,13 @@ impl Methods {
     ) -> Html<String> {
         let _created = AllocationStore::new(app_state).create_or_update(allocation.clone()).await;
 
-        Html(format!(
-            "<tr id='allocation-{}-{}'><td>{}</td><td>{}</td><td>{}</td><td><button hx-delete='/allocations/{}/{}' hx-target='#allocation-{}-{}'>Delete</button></td></tr>",
-            allocation.scheme_name, allocation.symbol, allocation.scheme_name, allocation.symbol, allocation.amount, allocation.scheme_name, allocation.symbol, allocation.scheme_name, allocation.symbol
-        ))
+        let row_template = utils::get_file_text("row_allocation.html").await;
+        let row_html = row_template
+            .replace("{scheme_name}", &allocation.scheme_name)
+            .replace("{symbol}", &allocation.symbol)
+            .replace("{amount}", &allocation.amount.to_string());
+
+        Html(row_html)
     }
 
     pub async fn remove(
--- src/routers/index/mod.rs
+++ src/routers/index/mod.rs
@@ -1,11 +1,15 @@
 use axum::response::Html;
 use axum::Router;
 use axum::routing::get;
+use tower_http::services::ServeDir;
 use crate::routers::utils;
 
 pub fn get_router() -> Router {
+    let assets = ServeDir::new("assets");
+
     Router::new()
         .route("/", get(serve_html_file))
+        .nest_service("/assets", assets)
 }
 
 async fn serve_html_file() -> Html<String> {
